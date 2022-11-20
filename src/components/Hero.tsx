1002588c79893af1036a245b151ba9c8b2bb4f15
marcuszierke <marcuszierke@gmail.com>
2022-11-20 18:42:23 +0100

    feat: header and navBar added | mz

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,5 +3,12 @@ import { useTranslation } from 'react-i18next';
 export const Hero = () => {
 	const { t } = useTranslation();
 
-	return <div className='hero'>{t('heroTitle')}</div>;
+	return (
+		<div className='hero'>
+			<div className='hero-content'>
+				<h1>{t('hero.title')}</h1>
+				<p>{t('hero.text')}</p>
+			</div>
+		</div>
+	);
 };
