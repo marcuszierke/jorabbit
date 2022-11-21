72661903d455e1c64bf295c6bf59df0e5f37bbef
marcuszierke <marcuszierke@gmail.com>
2022-11-21 14:34:06 +0100

    feat: Feedback added | mz

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -1,13 +1,17 @@
 import { useTranslation } from 'react-i18next';
+import { Headline } from './Headline';
 
 export const Feedback = () => {
 	const { t } = useTranslation();
 
 	return (
-		<div className='about'>
-			<div id='about'>
-				<div className='link'>{t('about.link')}</div>
-				<p>{t('about.text')}</p>
+		<div className='feedback'>
+			<div id='feedback'>
+				<div className='link'>{t('feedback.link')}</div>
+				<p>
+					<Headline>{t('feedback.highlight')}</Headline> {t('feedback.text')}
+				</p>
+				<p className='source'>{t('feedback.source')}</p>
 			</div>
 		</div>
 	);
