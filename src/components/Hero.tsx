999510bae96c434ff3882d2ba3c9be611da8e669
marcuszierke <marcuszierke@gmail.com>
2023-07-05 23:59:14 +0200

    animation in hero added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,21 @@
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+import heroVideo from "../assets/hero_animation.mp4";
 
 export const Hero = () => {
-	const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
-	return (
-		<div className='hero' lang={i18n.language}>
-			<div className='hero-content'>
-				<div>
-					<h1>{t('hero.title')}</h1>
-					<div style={{ flex: 1 }}></div>
-				</div>
-				<p>{t('hero.text')}</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="hero" lang={i18n.language}>
+      <video className="video-background" autoPlay loop muted playsInline>
+        <source src={heroVideo} type="video/mp4" />
+      </video>
+      <div className="hero-content">
+        <div>
+          <h1>{t("hero.title")}</h1>
+          <div style={{ flex: 1 }}></div>
+        </div>
+        <p>{t("hero.text")}</p>
+      </div>
+    </div>
+  );
 };
