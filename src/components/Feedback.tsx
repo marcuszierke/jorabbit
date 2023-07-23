348d44965a08cc285ddf0df30dcffc4477615fd6
marcuszierke <marcus.zierke@she.net>
2023-07-23 16:21:58 +0200

    hero animation replaced

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -1,17 +1,17 @@
-import { useTranslation } from 'react-i18next';
-import { Headline } from './Headline';
+import { useTranslation } from "react-i18next";
+import { Headline } from "./Headline";
 
 export const Feedback = () => {
-	const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
-	return (
-		<div className='feedback'>
-			<div className='link'>{t('feedback.link')}</div>
-			<div className='content' lang={i18n.language}>
-				<Headline>{t('feedback.highlight')}</Headline>
-				<div className='quote'>{t('feedback.text')}</div>
-				<div className='source'>{t('feedback.source')}</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="feedback">
+      <div className="link">{t("feedback.link")}</div>
+      <div className="content" lang={i18n.language}>
+        <Headline>{t("feedback.highlight")}</Headline>
+        <div className="quote">{t("feedback.text")}</div>
+        <div className="source">{t("feedback.source")}</div>
+      </div>
+    </div>
+  );
 };
--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,12 +1,12 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 type Props = {
-	children: ReactNode;
+  children: ReactNode;
 };
 export const Headline = ({ children }: Props) => (
-	<div className='headline'>
-		<h2 className='gradient padding'>
-			<span>{children}</span>
-		</h2>
-	</div>
+  <div className="headline">
+    <h2 className="gradient padding">
+      <span>{children}</span>
+    </h2>
+  </div>
 );
